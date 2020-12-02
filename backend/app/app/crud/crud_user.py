285c7448dcd297726c4159bca65c0abe55987ef9
Akhil Narang <me@akhilnarang.dev>
2020-12-03 01:31:43 +0530

    crud_user: update: switch to assignment expression
    I would've done so in the initial commit, but PyCharm thought I was
    using python3.7 and not 3.8 for some reason...
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- backend/app/app/crud/crud_user.py
+++ backend/app/app/crud/crud_user.py
@@ -29,8 +29,8 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             update_data = obj_in
         else:
             update_data = obj_in.dict(exclude_unset=True)
-        if update_data.get("password"):
-            hashed_password = get_password_hash(update_data["password"])
+        if password := update_data.get("password"):
+            hashed_password = get_password_hash(password)
             del update_data["password"]
             update_data["hashed_password"] = hashed_password
         return super().update(db, db_obj=db_obj, obj_in=update_data)
