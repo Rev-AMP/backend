aff51d6e34372ca4e5ac54c6c8a28f16688aef18
ksdfg <ksdfg123@gmail.com>
2020-12-03 15:59:06 +0530

    Add tests for
    - Updating Normal User
    - Fetching Normal User
    
    alongside the tests for Superusers.
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- backend/app/app/tests/crud/test_user.py
+++ backend/app/app/tests/crud/test_user.py
@@ -72,6 +72,17 @@ def test_check_if_user_is_superuser_normal_user(db: Session) -> None:
 def test_get_user(db: Session) -> None:
     password = random_lower_string()
     username = random_email()
+    user_in = UserCreate(email=username, password=password)
+    user = crud.user.create(db, obj_in=user_in)
+    user_2 = crud.user.get(db, id=user.id)
+    assert user_2
+    assert user.email == user_2.email
+    assert jsonable_encoder(user) == jsonable_encoder(user_2)
+
+
+def test_get_superuser(db: Session) -> None:
+    password = random_lower_string()
+    username = random_email()
     user_in = UserCreate(email=username, password=password, is_superuser=True)
     user = crud.user.create(db, obj_in=user_in)
     user_2 = crud.user.get(db, id=user.id)
@@ -81,6 +92,20 @@ def test_get_user(db: Session) -> None:
 
 
 def test_update_user(db: Session) -> None:
+    password = random_lower_string()
+    email = random_email()
+    user_in = UserCreate(email=email, password=password)
+    user = crud.user.create(db, obj_in=user_in)
+    new_password = random_lower_string()
+    user_in_update = UserUpdate(password=new_password)
+    crud.user.update(db, db_obj=user, obj_in=user_in_update)
+    user_2 = crud.user.get(db, id=user.id)
+    assert user_2
+    assert user.email == user_2.email
+    assert verify_password(new_password, user_2.hashed_password)
+
+
+def test_update_superuser(db: Session) -> None:
     password = random_lower_string()
     email = random_email()
     user_in = UserCreate(email=email, password=password, is_superuser=True)
