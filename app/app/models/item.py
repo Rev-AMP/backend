91c110b66ec14068b3511be62a0173ef9522d419
ksdfg <ksdfg123@gmail.com>
2020-12-04 19:46:35 +0530

    Remove  from item model
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/app/models/item.py
+++ app/app/models/item.py
@@ -14,4 +14,3 @@ class Item(Base):
     title = Column(String, index=True)
     description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("user.id"))
-    owner = relationship("User", back_populates="items")
