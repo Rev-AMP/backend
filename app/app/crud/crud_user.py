e76cd1fa17ff161df333489c1764100a49f66945
ksdfg <ksdfg123@gmail.com>
2020-12-04 19:24:53 +0530

    Update User model
    - add profile_picture in create
    - remove is_superuser attribute and replace it with type attribute
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/app/crud/crud_user.py
+++ app/app/crud/crud_user.py
@@ -17,7 +17,8 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             email=obj_in.email,
             hashed_password=get_password_hash(obj_in.password),
             full_name=obj_in.full_name,
-            is_superuser=obj_in.is_superuser,
+            type=obj_in.type,
+            profile_picture=obj_in.profile_picture,
         )
         db.add(db_obj)
         db.commit()
@@ -47,7 +48,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         return user.is_active
 
     def is_superuser(self, user: User) -> bool:
-        return user.is_superuser
+        return user.type == "superuser"
 
 
 user = CRUDUser(User)
--- app/app/models/user.py
+++ app/app/models/user.py
@@ -1,17 +1,14 @@
-from typing import TYPE_CHECKING
-
 from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.dialects.postgresql import ENUM
 
 from app.db.base_class import Base
-
-if TYPE_CHECKING:
-    from .item import Item  # noqa: F401
 
 
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
+    profile_picture = Column(String, default="")
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
-    is_superuser = Column(Boolean(), default=False)
+    type = Column(ENUM("superuser", "student"), nullable=False)
