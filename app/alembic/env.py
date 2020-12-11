970cfef5f52fb887a3deb87b0cd6db8313bbb36a
Akhil Narang <me@akhilnarang.dev>
2020-12-11 14:54:15 +0530

    alembic: env: don't print config file name
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -11,7 +11,6 @@ from app.db.base import Base  # noqa
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-print(config.config_file_name)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
