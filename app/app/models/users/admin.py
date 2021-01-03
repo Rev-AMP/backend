562ac0a9a28346e6e1f72ffea334d97cc5e720e8
Aditya Desai <aditya.desai0199@gmail.com>
2021-01-03 18:51:40 +0530

    Add sqlalchemy model class for Admin user

---
+++ app/app/models/users/admin.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, ForeignKey, Integer
+
+from app.db.base_class import Base
+from app.models.users.user import User
+
+
+class Admin(Base):
+    user_id = Column(Integer, ForeignKey(f'{User.__table__.name}.id'), primary_key=True, index=True)
+    permissions = Column(Integer, nullable=False, default=0)
