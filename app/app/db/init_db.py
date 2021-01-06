bde01b7f1a59d051f8090875d9436792eb7cdc34
Akhil Narang <me@akhilnarang.dev>
2021-01-06 18:59:21 +0530

    init_db: superuser is an admin
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/db/init_db.py
+++ app/app/db/init_db.py
@@ -18,8 +18,6 @@ def init_db(db: Session) -> None:
     user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
     if not user:
         user_in = schemas.UserCreate(
-            email=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            type="superuser",
+            email=settings.FIRST_SUPERUSER, password=settings.FIRST_SUPERUSER_PASSWORD, type="superuser", is_admin=True
         )
         user = crud.user.create(db, obj_in=user_in)  # noqa: F841
