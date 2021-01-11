Rev-AMP/backend

commit ad60277f499fe996b21b7b13e3b9279a6362a1d4

backend: add an `is_admin` field to User
This will allow us to easily handle cases where a professor is also an admin

Signed-off-by: Akhil Narang <me@akhilnarang.dev>

app/app/crud/crud_user.py
@@ -61,8 +61,8 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def is_active(self, user: User) -> bool:
         return user.is_active
 
-    def is_admin(self, user: User) -> bool:
-        return user.type == "admin"
+    def check_admin(self, user: User) -> bool:
+        return user.type == "admin" or user.is_admin
 
     def is_superuser(self, user: User) -> bool:
         return user.type == "superuser"

app/alembic/versions/2f4521008b5b_added_admin_models.py
@@ -25,6 +25,7 @@ def upgrade():
     sa.PrimaryKeyConstraint('user_id')
     )
     op.create_index(op.f('ix_admin_user_id'), 'admin', ['user_id'], unique=False)
+    op.add_column('user', sa.Column('is_admin', sa.Boolean(), nullable=True))
     # ### end Alembic commands ###
 
 
@@ -32,4 +33,5 @@ def downgrade():
     # ### commands auto generated by Alembic - please adjust! ###
     op.drop_index(op.f('ix_admin_user_id'), table_name='admin')
     op.drop_table('admin')
+    op.drop_column('user', 'is_admin')
     # ### end Alembic commands ###

app/app/api/api_v1/endpoints/admins.py
@@ -53,19 +53,17 @@ def create_admin(
         )
 
     # Ensure the user is not a superuser
-    if user.type == "superuser":
+    if user.type != "professor":
         raise HTTPException(
             status_code=400,
-            detail="This user is a superuser!",
+            detail="This user cannot be promoted to an admin!",
         )
 
-    # Ensure the user is an admin - change type if required
-    if user.type != "admin":
-        user_in = schemas.UserUpdate(id=user.id, type="admin")
-        # TODO: Delete user from student/professor if so required
-        crud.user.update(db, db_obj=user, obj_in=user_in)
+    # Set is_admin to true so that we don't need to change user type
+    user_in = schemas.UserUpdate(id=user.id, is_admin=True)
+    crud.user.update(db, db_obj=user, obj_in=user_in)
 
-    # Create new admin
+    # Create new admin object
     admin = crud.admin.create(db, obj_in=admin_in)
 
     if settings.EMAILS_ENABLED:

app/app/api/deps.py
@@ -48,7 +48,7 @@ def get_current_active_user(
 def get_current_active_admin(
     current_user: models.User = Depends(get_current_user),
 ) -> models.User:
-    if not crud.user.is_admin(current_user) and not crud.user.is_superuser(current_user):
+    if not crud.user.check_admin(current_user) and not crud.user.is_superuser(current_user):
         raise HTTPException(status_code=400, detail="The user doesn't have enough privileges")
     return current_user
 

app/app/models/users/user.py
@@ -16,4 +16,5 @@ class User(Base):
     profile_picture = Column(String(4096), default=None, nullable=True)
     hashed_password = Column(String(100), nullable=False)
     is_active = Column(Boolean(), default=True)
+    is_admin = Column(Boolean(), default=False)
     type = Column(ENUM("superuser", "student", "professor", "admin", name="user_type"), nullable=False)

app/app/schemas/users/user.py
@@ -10,6 +10,7 @@ class UserBase(BaseModel):
     type: Optional[str] = None
     full_name: Optional[str] = None
     profile_picture: Optional[str] = None
+    is_admin: Optional[bool] = False
 
 
 # Properties to receive via API on creation

app/app/tests/api/api_v1/test_admins.py
@@ -59,7 +59,7 @@ def test_create_admin_existing_nonadmin(client: TestClient, superuser_token_head
     updated_user = crud.user.get(db, admin_id)
     assert 200 <= r.status_code < 300
     assert updated_user
-    assert updated_user.type == "admin"
+    assert updated_user.is_admin
     assert "user_id" in created_admin
     assert "permissions" in created_admin
 

commit 030e21cd2395ca17a6141a33a8df66396114d6c7

crud_user: fix huge mistake introduced in ad60277f499fe996b21b7b13e3b9279a6362a1d4
This resulted in is_admin=True having no effect

Signed-off-by: Akhil Narang <me@akhilnarang.dev>

app/app/crud/crud_user.py
@@ -21,6 +21,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             full_name=obj_in.full_name,
             type=obj_in.type,
             profile_picture=obj_in.profile_picture,
+            is_admin=obj_in.is_admin,
         )
         db.add(db_obj)
 