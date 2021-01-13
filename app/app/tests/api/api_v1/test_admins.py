ab7c895174cb75840c7eee6f7273488646782a87
ksdfg <ksdfg123@gmail.com>
2021-01-13 18:29:26 +0530

    app/tests/api/api_v1/test_admins.py: add more tests for admin
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/app/tests/api/api_v1/test_admins.py
+++ app/app/tests/api/api_v1/test_admins.py
@@ -37,23 +37,6 @@ def test_create_admin_existing_id(client: TestClient, superuser_token_headers: d
     assert "user_id" not in created_user
 
 
-def test_update_admin(client: TestClient, superuser_token_headers: dict, db: Session) -> None:
-    username = random_email()
-    # username = email
-    password = random_lower_string()
-    user_in = UserCreate(email=username, password=password, type="admin")
-    admin_id = crud.user.create(db, obj_in=user_in).id
-    data = {"user_id": admin_id, "permissions": 5}
-    r = client.put(
-        f"{settings.API_V1_STR}/admins/",
-        headers=superuser_token_headers,
-        json=data,
-    )
-    updated_admin = r.json()
-    assert 200 <= r.status_code < 300
-    assert updated_admin["permissions"] == 5
-
-
 def test_create_admin_existing_nonadmin(client: TestClient, superuser_token_headers: dict, db: Session) -> None:
     username = random_email()
     # username = email
@@ -76,6 +59,65 @@ def test_create_admin_existing_nonadmin(client: TestClient, superuser_token_head
     assert "permissions" in created_admin
 
 
+def test_create_admin_existing_student(client: TestClient, superuser_token_headers: dict, db: Session) -> None:
+    username = random_email()
+    # username = email
+    password = random_lower_string()
+    user_in = UserCreate(email=username, password=password, type="student")
+    user = crud.user.create(db, obj_in=user_in)
+    admin_id = user.id
+    data = {"user_id": admin_id, "permissions": 0}
+    r = client.post(
+        f"{settings.API_V1_STR}/admins/",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    created_admin = r.json()
+    db.refresh(user)
+    assert r.status_code == 400
+    assert user
+    assert not user.is_admin
+    assert "user_id" not in created_admin
+
+
+def test_update_admin(client: TestClient, superuser_token_headers: dict, db: Session) -> None:
+    username = random_email()
+    # username = email
+    password = random_lower_string()
+    user_in = UserCreate(email=username, password=password, type="admin")
+    admin_id = crud.user.create(db, obj_in=user_in).id
+    data = {"user_id": admin_id, "permissions": 5}
+    r = client.put(
+        f"{settings.API_V1_STR}/admins/",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    updated_admin = r.json()
+    assert 200 <= r.status_code < 300
+    assert updated_admin["permissions"] == 5
+
+
+def test_update_admin_nonadmin(client: TestClient, superuser_token_headers: dict, db: Session) -> None:
+    username = random_email()
+    # username = email
+    password = random_lower_string()
+    user_in = UserCreate(email=username, password=password, type="student")
+    user = crud.user.create(db, obj_in=user_in)
+    admin_id = user.id
+    data = {"user_id": admin_id, "permissions": 5}
+    r = client.put(
+        f"{settings.API_V1_STR}/admins/",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    updated_admin = r.json()
+    assert r.status_code == 400
+    db.refresh(user)
+    assert user
+    assert not user.is_admin
+    assert 'user_id' not in updated_admin
+
+
 def test_remove_admin(client: TestClient, superuser_token_headers: dict, db: Session) -> None:
     username = random_email()
     password = random_lower_string()
@@ -90,6 +132,7 @@ def test_remove_admin(client: TestClient, superuser_token_headers: dict, db: Ses
     )
     db.refresh(user)
     assert 200 <= r.status_code < 300
+    assert user
     assert not user.is_admin
     assert not crud.admin.get(db, id=admin_id)
 
@@ -108,6 +151,7 @@ def test_remove_admin_nonadmin(client: TestClient, superuser_token_headers: dict
     )
     db.refresh(user)
     assert r.status_code == 400
+    assert user
     assert not user.is_admin
 
 
@@ -118,6 +162,7 @@ def test_remove_admin_nonuser(client: TestClient, superuser_token_headers: dict,
     user = crud.user.create(db, obj_in=user_in)
     admin_id = user.id
     crud.user.remove(db, id=admin_id)
+    assert not crud.user.get(db, id=admin_id)
     data = {"user_id": admin_id}
     r = client.delete(
         f"{settings.API_V1_STR}/admins/",
