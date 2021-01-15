55b1438170d5c45e62eb0d58115fd18ada0f3357
ksdfg <ksdfg123@gmail.com>
2021-01-15 14:09:40 +0530

    app/api/api_v1/endpoints/schools.py: add basic CRUD API endpoints
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/app/schemas/__init__.py
+++ app/app/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .msg import Msg
+from .school import School, SchoolCreate, SchoolUpdate
 from .token import Token, TokenPayload
 from .users.admin import Admin, AdminCreate, AdminRemove, AdminUpdate
 from .users.user import User, UserCreate, UserInDB, UserUpdate
