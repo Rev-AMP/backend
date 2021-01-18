7b79ec00544cbfdad88b1b8d46a2880a6b80cc17
ksdfg <ksdfg123@gmail.com>
2021-01-18 23:22:01 +0530

    app/api/deps.py: update status code to 403 for 'Inactive user' error
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/app/api/deps.py
+++ app/app/api/deps.py
@@ -41,7 +41,7 @@ def get_current_active_user(
     current_user: models.User = Depends(get_current_user),
 ) -> models.User:
     if not crud.user.is_active(current_user):
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=403, detail="Inactive user")
     return current_user
 
 
