74434a89d727b6550cd19aea60e36709b14fd164
Akhil Narang <me@akhilnarang.dev>
2021-02-09 18:25:25 +0530

    models: school: made head non-nullable
    Add the alembic revision for the same
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/models/school.py
+++ app/app/models/school.py
@@ -6,4 +6,4 @@ from app.db.base_class import Base
 class School(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), unique=True, index=True, nullable=False)
-    head = Column(String(100), unique=True)
+    head = Column(String(100), unique=True, index=True, nullable=False)
