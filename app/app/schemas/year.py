323530d9ed5c9095377ea0d0b833408bc46d1a75
Akhil Narang <me@akhilnarang.dev>
2021-02-13 17:51:53 +0530

    schemas: year: move validations to YearBase
    Change type of start_year and end_year in the validators
    Optional[int] has been used since in the case of YearUpdate,
    there are chances that these values will not be provided.
    
    Update the checks to accordingly ensure that a value has been given,
    and only then validate and update it.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/schemas/year.py
+++ app/app/schemas/year.py
@@ -11,27 +11,32 @@ class YearBase(BaseModel):
     end_year: Optional[int] = None
     is_active: Optional[bool] = True
 
+    @validator('start_year')
+    def validate_start_year(cls, start_year: Optional[int]) -> Optional[int]:
+        if start_year:
+            current_year = datetime.now().year
+            if start_year > current_year + 1:
+                raise ValueError("You can't work so much in the future!")
+            if start_year < current_year - 1:
+                raise ValueError("You can't work so much in the past!")
+        return start_year
+
+    @validator('end_year')
+    def validate_end_year(cls, end_year: Optional[int], values: dict) -> Optional[int]:
+        if end_year:
+            if values.get('start_year'):
+                if end_year < values['start_year']:
+                    raise ValueError("You can't end the year before it starts")
+            if end_year < datetime.now().year - 1:
+                raise ValueError("You can't work so much in the past!")
+        return end_year
+
 
 # Properties to receive via API on creation
 class YearCreate(YearBase):
     school_id: int
     start_year: int
     end_year: int
-
-    @validator('start_year')
-    def validate_start_year(cls, start_year: int) -> int:
-        current_year = datetime.now().year
-        if start_year > current_year + 1:
-            raise ValueError("You can't work so much in the future!")
-        if start_year < current_year:
-            raise ValueError("You can't work so much in the past!")
-        return current_year
-
-    @validator('end_year')
-    def validate_end_year(cls, end_year: int, values: dict) -> int:
-        if end_year < values['start_year']:
-            raise ValueError("You can't end the year before it starts")
-        return end_year
 
 
 # Properties to receive via API on update
