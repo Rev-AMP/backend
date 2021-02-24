8515ffa4fddd8ce9d9bc9cb1080b82258b468c40
Aditya Desai <aditya.desai0199@gmail.com>
2021-02-24 18:32:53 +0530

    tests/utils/term: Add function to create random Term

---
+++ app/app/tests/utils/term.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from random import randint
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from sqlalchemy.types import Date
+
+from app import crud
+from app.models import Term
+from app.schemas import TermCreate
+
+from .year import create_random_year
+from .utils import random_lower_string
+
+
+def create_random_term(
+    db: Session,
+    name: Optional[str] = None,
+    year_id: Optional[int] = None,
+    current_year_term: Optional[int] = None,
+    start_date: Optional[Date] = None,
+    end_date: Optional[Date] = None,
+    has_electives: Optional[bool] = False,
+    is_active: Optional[bool] = True,
+) -> Term:
+    """
+    Create a random term, or based on given inputs
+    """
+
+    if year_id is None:
+        year_id = create_random_year(db).id
+
+    return crud.term.create(
+        db=db,
+        obj_in=TermCreate(
+            name=name or random_lower_string(),
+            year_id=year_id,
+            current_year_term=current_year_term or randint(1, 3),
+            start_date=start_date or datetime.now().date(),
+            end_date=end_date or datetime.now().date(),
+            has_electives=has_electives,
+            is_active=is_active,
+        ),
+    )
