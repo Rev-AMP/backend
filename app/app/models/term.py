43dc299d732b347c21a9490589ab5f83966970bb
Aditya Desai <aditya.desai0199@gmail.com>
2021-02-25 17:59:56 +0530

    how the **** did I miss id

--- app/app/models/term.py
+++ app/app/models/term.py
@@ -5,9 +5,10 @@ from app.models.year import Year
 
 
 class Term(Base):
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), index=True)
-    year_id = Column(Integer, ForeignKey(f'{Year.__table__.name}.id', ondelete='CASCADE'), primary_key=True)
-    current_year_term = Column(Integer, primary_key=True)
+    year_id = Column(Integer, ForeignKey(f'{Year.__table__.name}.id', ondelete='CASCADE'), index=True, nullable=False)
+    current_year_term = Column(Integer)
     start_date = Column(Date, index=True, nullable=False)
     end_date = Column(Date, index=True, nullable=True)
     has_electives = Column(Boolean, default=False)
