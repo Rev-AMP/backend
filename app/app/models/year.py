c0da779686b9fe858be50c9c70df6b8dc895a8da
ksdfg <ksdfg123@gmail.com>
2021-03-07 18:30:30 +0530

    models: year: make important fields non nullable and add unique constraint
    - Since schema enforces `name`, `school_id`, `start_year` and `end_year` to never be None, enforce the same in the model to err on the side of caution.
    
    - Add a unique constraint on `name`, `school_id`, `start_year` and `end_year` together to make sure each Year object is identifiable by those four attributes by a user.
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/app/models/year.py
+++ app/app/models/year.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, UniqueConstraint
 
 from app.db.base_class import Base
 from app.models import School
@@ -6,8 +6,12 @@ from app.models import School
 
 class Year(Base):
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), unique=True, index=True, nullable=False)
-    school_id = Column(Integer, ForeignKey(f'{School.__table__.name}.id', ondelete='CASCADE'), index=True)
-    start_year = Column(Integer, index=True)
-    end_year = Column(Integer, index=True)
+    name = Column(String(100), index=True, nullable=False)
+    school_id = Column(
+        Integer, ForeignKey(f'{School.__table__.name}.id', ondelete='CASCADE'), index=True, nullable=False
+    )
+    start_year = Column(Integer, index=True, nullable=False)
+    end_year = Column(Integer, index=True, nullable=False)
     is_active = Column(Boolean, default=True)
+
+    __table_args__ = (UniqueConstraint("name", "school_id", "start_year", "end_year"),)
