00c77cbb5905f2a5c30fd1e49fe1347161922fd3
Akhil Narang <me@akhilnarang.dev>
2021-03-07 20:38:45 +0530

    Don't use alembic for non MySQL setups
    We've configured these revisions with and for MySQL, many of them
    are not compatible with other RDBMS, like the PostgreSQL setup we
    have to run in docker. Since that is basically just used for tests
    in GitHub Actions, we always create a fresh DB anyway, so just
    directly initialize it.
    
    Also, remove the alembic upgrade step from prestart.sh, since again
    that is just used by the above action.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/db/init_db.py
+++ app/app/db/init_db.py
@@ -1,8 +1,11 @@
+from decouple import config
 from sqlalchemy.orm import Session
 
 from app import crud, schemas
 from app.core.config import settings
 from app.db import base  # noqa: F401
+from app.db.base_class import Base
+from app.db.session import engine
 
 # make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly
@@ -13,7 +16,8 @@ def init_db(db: Session) -> None:
     # Tables should be created with Alembic migrations
     # But if you don't want to use migrations, create
     # the tables un-commenting the next line
-    # Base.metadata.create_all(bind=engine)
+    if config('DB', default='mysql') != 'mysql':
+        Base.metadata.create_all(bind=engine)
 
     user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
     if not user:
