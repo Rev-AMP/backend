43e07c98db218f3d3a301838c5f755186ab33030
ksdfg <ksdfg123@gmail.com>
2021-03-19 01:25:17 +0530

    api: admins: read_admins: update name from get_admin and update description
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/app/api/api_v1/endpoints/admins.py
+++ app/app/api/api_v1/endpoints/admins.py
@@ -10,14 +10,14 @@ router = APIRouter()
 
 
 @router.get("/", response_model=List[schemas.Admin])
-def get_admin(
+def read_admins(
     db: Session = Depends(deps.get_db),
     skip: int = 0,
     limit: int = 100,
     _: models.Admin = Depends(deps.get_current_active_admin_with_permission("admin")),
 ) -> Any:
     """
-    Get current admin
+    Retrieve admins
     """
     return crud.admin.get_multi(db, skip=skip, limit=limit)
 
