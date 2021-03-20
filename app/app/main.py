d5d6970e74154d21902bcdbcac5d4da9addf7fdc
Akhil Narang <me@akhilnarang.dev>
2021-03-20 11:26:40 +0530

    Add initial logging configuration
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/main.py
+++ app/app/main.py
@@ -1,8 +1,18 @@
+import logging
+import sys
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
+
+logging.basicConfig(
+    format="[%(levelname)s] (%(asctime)s) %(module)s:%(pathname)s:%(funcName)s:%(lineno)s:: %(message)s",
+    level=logging.INFO,
+    datefmt="%d-%m-%y %H:%M:%S",
+    stream=sys.stdout,
+)
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 
