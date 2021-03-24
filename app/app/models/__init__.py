c39ebcea3b43c4bce3acbd4903b9a0afaf68b176
Akhil Narang <me@akhilnarang.dev>
2021-03-24 18:24:09 +0530

    Add in student model
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/models/__init__.py
+++ app/app/models/__init__.py
@@ -2,5 +2,6 @@ from .course import Course
 from .school import School
 from .term import Term
 from .users.admin import Admin
+from .users.student import Student
 from .users.user import User
 from .year import Year
---
+++ app/app/models/users/student.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+from app.models.term import Term
+from app.models.users.user import User
+
+
+class Student(Base):
+    user_id = Column(
+        Integer,
+        ForeignKey(f'{User.__table__.name}.id', ondelete='CASCADE'),
+        index=True,
+        primary_key=True,
+        nullable=False,
+    )
+    user = relationship("User")
+    term_id = Column(Integer, ForeignKey(f'{Term.__table__.name}.id', ondelete='CASCADE'), index=True, nullable=True)
