d04eff91bd392fa49d94ea02445c5840850b8220
Akhil Narang <me@akhilnarang.dev>
2021-03-24 18:24:43 +0530

    Add in student test utils
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

---
+++ app/app/tests/utils/student.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from app import crud
+from app.models.users.student import Student
+from app.schemas import StudentUpdate
+from app.tests.utils.term import create_random_term
+from app.tests.utils.user import create_random_user
+
+
+def create_random_student(db: Session, term_id: Optional[int] = None) -> Student:
+    user = create_random_user(db, type='student')
+    student = crud.student.get(db, id=user.id)
+    assert student
+    if term_id and crud.term.get(db, id=term_id):
+        student_in = StudentUpdate(user_id=user.id, term_id=term_id)
+    else:
+        term = create_random_term(db)
+        student_in = StudentUpdate(user_id=user.id, term_id=term.id)
+    return crud.student.update(db, db_obj=student, obj_in=student_in)
