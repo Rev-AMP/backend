89d4c39b1b83cc230afae805bde86a40932aba60
Akhil Narang <me@akhilnarang.dev>
2021-03-24 18:24:15 +0530

    Add in student schema
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/schemas/__init__.py
+++ app/app/schemas/__init__.py
@@ -4,5 +4,6 @@ from .school import School, SchoolCreate, SchoolUpdate
 from .term import Term, TermCreate, TermInDB, TermUpdate
 from .token import Token, TokenPayload
 from .users.admin import Admin, AdminCreate, AdminPermissions, AdminRemove, AdminUpdate
+from .users.student import Student, StudentCreate, StudentRemove, StudentUpdate
 from .users.user import User, UserCreate, UserInDB, UserUpdate
 from .year import Year, YearCreate, YearInDB, YearUpdate
---
+++ app/app/schemas/users/student.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+from pydantic import BaseModel, validator
+
+from .user import User
+
+
+# Shared properties
+class StudentBase(BaseModel):
+    user_id: Optional[int] = None
+    term_id: Optional[int]
+
+    @validator('user_id')
+    def user_not_empty(cls, user_id: Optional[int]) -> Optional[int]:
+        if user_id is None:
+            raise ValueError("Course name must not be empty!")
+        if user_id == 0:
+            raise ValueError("Invalid user id!")
+        return user_id
+
+
+# Properties to receive via API on creation
+class StudentCreate(StudentBase):
+    user_id: int
+
+
+# Properties to receive via API on update
+class StudentUpdate(StudentBase):
+    pass
+
+
+# Properties to receive via API on remove
+class StudentRemove(StudentBase):
+    user_id: int
+
+
+# Additional properties to return through API
+class Student(StudentBase):
+    user: Optional[User]
+
+    class Config:
+        orm_mode = True
