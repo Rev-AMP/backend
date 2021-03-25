5fee5b89925e32f41ef59b72d41170876f858f5c
Akhil Narang <me@akhilnarang.dev>
2021-03-25 13:42:26 +0530

    Fix create_random_student() test breakage
    I don't know how or why it fixes it - but creating student before
    term seems to kill the student object's attributes once the term is
    created.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/tests/utils/student.py
+++ app/app/tests/utils/student.py
@@ -11,11 +11,11 @@ from app.tests.utils.user import create_random_user
 
 def create_random_student(db: Session, term_id: Optional[int] = None) -> Student:
     user = create_random_user(db, type='student')
-    student = crud.student.get(db, id=user.id)
-    assert student
     if term_id and crud.term.get(db, id=term_id):
         student_in = StudentUpdate(user_id=user.id, term_id=term_id)
     else:
         term = create_random_term(db)
         student_in = StudentUpdate(user_id=user.id, term_id=term.id)
+    student = crud.student.get(db, id=user.id)
+    assert student
     return crud.student.update(db, db_obj=student, obj_in=student_in)
