18cf1c0218edab35ba4e534f40d5197dc8c8f7e6
Aditya Desai <aditya.desai0199@gmail.com>
2021-03-31 18:27:16 +0530

    Add professor model

--- app/app/models/__init__.py
+++ app/app/models/__init__.py
@@ -2,6 +2,7 @@ from .course import Course
 from .school import School
 from .term import Term
 from .users.admin import Admin
+from .users.professor import Professor
 from .users.student import Student
 from .users.user import User
 from .year import Year
---
+++ app/app/models/users/professor.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+from app.models.users.user import User
+
+
+class Professor(Base):
+    user_id = Column(
+        Integer,
+        ForeignKey(f"{User.__table__.name}.id", ondelete="CASCADE"),
+        index=True,
+        primary_key=True,
+    )
+    user = relationship("User")
