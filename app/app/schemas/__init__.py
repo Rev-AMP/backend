356e774583c0dc8e98a82296be201f2cb4b363e8
Aditya Desai <aditya.desai0199@gmail.com>
2021-03-31 18:24:47 +0530

    Add professor schema

--- app/app/schemas/__init__.py
+++ app/app/schemas/__init__.py
@@ -4,6 +4,7 @@ from .school import School, SchoolCreate, SchoolUpdate
 from .term import Term, TermCreate, TermInDB, TermUpdate
 from .token import Token, TokenPayload
 from .users.admin import Admin, AdminCreate, AdminPermissions, AdminRemove, AdminUpdate
+from .users.professor import Professor, ProfessorCreate, ProfessorRemove, ProfessorUpdate
 from .users.student import Student, StudentCreate, StudentRemove, StudentUpdate
 from .users.user import User, UserCreate, UserInDB, UserUpdate
 from .year import Year, YearCreate, YearInDB, YearUpdate
