fe1c7a71b78e127288f5aa7f1c99fc252d01f42f
Aditya Desai <aditya.desai0199@gmail.com>
2021-03-31 18:38:09 +0530

    Add function for getting current professor object in deps.py

--- app/app/api/deps.py
+++ app/app/api/deps.py
@@ -71,6 +71,24 @@ def get_current_student(db: Session = Depends(get_db), user: models.User = Depen
     raise ForbiddenException(detail="User is not a student")
 
 
+def get_current_professor(db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)) -> models.Professor:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[security.ALGORITHM])
+        token_data = schemas.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise ForbiddenException(
+            detail="Could not validate credentials",
+        )
+
+    if user := crud.user.get(db, id=token_data.sub):
+        if user.type == "professor":
+            if professor := crud.professor.get(db, id=user.id):
+                return professor
+            raise NotFoundException(detail="Professor object not found")
+        raise ForbiddenException(detail="User is not a professor")
+    raise NotFoundException(detail="User not found")
+
+
 def get_current_admin_with_permission(permission: str) -> Callable:
     """
     Return a function that checks if the current active admin has permission for given task
