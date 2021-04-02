540f218ca6d2cfc800719aedbd8d12d96eb8e662
Akhil Narang <me@akhilnarang.dev>
2021-04-02 18:41:14 +0530

    Don't stop DB
    We have another run configuration to do this (which depends on the pytest config)
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/create-test-db-container.sh
+++ app/create-test-db-container.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-docker kill $(docker ps -q)
 docker run -e POSTGRES_USER=${POSTGRES_USER:?} -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD:?} -e POSTGRES_DB=${POSTGRES_DB:?} -p 5432:5432 -d postgres
 sleep 5s
 source ./${VENV:?}/bin/activate
