d73ceb57356ed7f296f4e5105cd27ddb4c7fa073
ksdfg <ksdfg123@gmail.com>
2021-04-02 19:44:09 +0530

    create-test-db-container.sh: add conditional actions for removing old containers and activating venv
    - If a container with the name `revamp_test_db` is still running, remove it
    - Activate venv only if env var VENV is set
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- app/create-test-db-container.sh
+++ app/create-test-db-container.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
-docker run -e POSTGRES_USER=${POSTGRES_USER:?} -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD:?} -e POSTGRES_DB=${POSTGRES_DB:?} -p 5432:5432 -d postgres
+
+set -e
+
+# remove test db container if exists
+if [ "$(docker ps -aq -f name=revamp_test_db)" ]; then
+  docker container rm -f revamp_test_db
+fi
+
+# create new test db container
+docker run --name revamp_test_db -e POSTGRES_USER="${POSTGRES_USER:?}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD:?}" -e POSTGRES_DB="${POSTGRES_DB:?}" -p 5432:5432 -d postgres
+
+# wait for container to initialize
 sleep 5s
-source ./${VENV:?}/bin/activate
+
+# activate venv if given
+if [ "${VENV}" ]; then
+  source "./${VENV}/bin/activate"
+fi
+
+# initialize test db with data
 python ./app/initial_data.py
