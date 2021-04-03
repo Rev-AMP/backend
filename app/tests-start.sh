76958fb05393111f3d8f4a979f7172d74d01b74e
Akhil Narang <me@akhilnarang.dev>
2021-04-03 15:46:42 +0530

    tests-start.sh: allow passing in the module you want tested
    By default run all
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/tests-start.sh
+++ app/tests-start.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 set -e
 
+module=${1:-app/tests}
+
 python app/initial_data.py
-pytest --cov=app --cov-report=term-missing app/tests
+pytest --cov=app --cov-report=term-missing ${module}
