63b93f83ea1c0733b7f2e480fe19a9d83515bc7f
Akhil Narang <me@akhilnarang.dev>
2021-04-03 12:18:31 +0530

    Update local test db container script
    No need to sleep now, the initial_data script retries DB connection 50 times.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/create-test-db-container.sh
+++ app/create-test-db-container.sh
@@ -2,21 +2,18 @@
 
 set -e
 
-# remove test db container if exists
+# Remove test db container if exists
 if [ "$(docker ps -aq -f name=revamp_test_db)" ]; then
   docker container rm -f revamp_test_db
 fi
 
-# create new test db container
+# Create new test db container
 docker run --name revamp_test_db -e POSTGRES_USER="${POSTGRES_USER:?}" -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD:?}" -e POSTGRES_DB="${POSTGRES_DB:?}" -p 5432:5432 -d postgres
 
-# wait for container to initialize
-sleep 5s
-
-# activate venv if given
+# Activate venv if given
 if [ "${VENV}" ]; then
   source "./${VENV}/bin/activate"
 fi
 
-# initialize test db with data
+# Initialize test db with data
 python ./app/initial_data.py
