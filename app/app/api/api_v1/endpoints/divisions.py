2b7474706a54303b51bee9d5d400a956b2821170
ksdfg <ksdfg123@gmail.com>
2021-04-03 21:10:00 +0530

    app: api: divisions: Check for `course` permissions to read divisions, not `division`
    That permission doesn't exist

--- app/app/api/api_v1/endpoints/divisions.py
+++ app/app/api/api_v1/endpoints/divisions.py
@@ -16,7 +16,7 @@ def read_divisions(
     db: Session = Depends(deps.get_db),
     skip: int = 0,
     limit: int = 100,
-    _: models.Admin = Depends(deps.get_current_admin_with_permission("division")),
+    _: models.Admin = Depends(deps.get_current_admin_with_permission("course")),
 ) -> Any:
     return crud.division.get_multi(db, skip=skip, limit=limit)
 
