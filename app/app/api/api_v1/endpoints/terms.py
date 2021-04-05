2eae4dcf7e28a7aa0fb56def6e34a9e8f093bca4
ksdfg <ksdfg123@gmail.com>
2021-04-05 23:40:07 +0530

    app: api: terms: add endpoint to bulk add students to term

--- app/app/api/api_v1/endpoints/terms.py
+++ app/app/api/api_v1/endpoints/terms.py
@@ -1,12 +1,13 @@
 import logging
 from typing import Any, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
 from app.api import deps
 from app.exceptions import ConflictException, NotFoundException
+from app.schemas import StudentUpdate
 
 router = APIRouter()
 
@@ -43,6 +44,65 @@ def read_term_students_by_id(
 ) -> Any:
     if term := crud.term.get(db, term_id):
         return term.students
+    raise NotFoundException(detail="The term with this ID does not exist!")
+
+
+@router.post("/{term_id}/students", response_model=List[schemas.Student])
+def read_term_students_by_id(
+    *,
+    db: Session = Depends(deps.get_db),
+    term_id: int,
+    user_ids: List[int],
+    _: models.Admin = Depends(deps.get_current_admin_with_permission("term")),
+) -> Any:
+    if term := crud.term.get(db, id=term_id):
+        errors = []
+
+        for user_id in user_ids:
+            if user := crud.user.get(db, id=user_id):
+                if user.type == "student":
+                    if user.school_id == term.year.school_id:
+                        if student := crud.student.get(db, id=user_id):
+                            crud.student.update(db, db_obj=student, obj_in=StudentUpdate(term_id=term_id))
+                        else:
+                            errors.append(
+                                {
+                                    "msg": "no student object",
+                                    "type": "user.no_student_object",
+                                    "loc": ["user ID", user_id],
+                                }
+                            )
+                    else:
+                        errors.append(
+                            {
+                                "msg": "different schools",
+                                "type": "objects.different_school",
+                                "loc": ["user ID", user_id],
+                            }
+                        )
+                else:
+                    errors.append(
+                        {
+                            "msg": "not a student",
+                            "type": "user.not_a_student",
+                            "loc": ["user ID", user_id],
+                        }
+                    )
+            else:
+                errors.append(
+                    {
+                        "msg": "not a user",
+                        "type": "user.not_a_user",
+                        "loc": ["user ID", user_id],
+                    }
+                )
+
+        if errors:
+            raise HTTPException(status_code=418, detail=errors)
+
+        db.refresh(term)
+        return term.students
+
     raise NotFoundException(detail="The term with this ID does not exist!")
 
 
