5a5ddf05251df9b7a710fe66cb6084206e44656a
ksdfg <ksdfg123@gmail.com>
2021-04-11 21:34:16 +0530

    app: models: fix small inconsistencies
    - rename division_student.py to student_division.py
    - make sure columns are not tuples

--- app/app/models/student_division.py
+++ app/app/models/student_division.py
@@ -7,9 +7,9 @@ from app.models.users.student import Student
 
 
 class StudentDivision(Base):
-    student_id = (Column("student_id", Integer, ForeignKey("student.user_id", ondelete="CASCADE"), primary_key=True),)
-    division_id = (Column("division_id", Integer, ForeignKey("division.id", ondelete="CASCADE"), primary_key=True),)
-    batch_number = (Column("batch_number", Integer, index=True),)
+    student_id = Column("student_id", Integer, ForeignKey("student.user_id", ondelete="CASCADE"), primary_key=True)
+    division_id = Column("division_id", Integer, ForeignKey("division.id", ondelete="CASCADE"), primary_key=True)
+    batch_number = Column("batch_number", Integer, index=True)
 
     student = relationship(Student, backref=backref("student_division", cascade="all, delete-orphan"))
     division = relationship(Division, backref=backref("student_division", cascade="all, delete-orphan"))
