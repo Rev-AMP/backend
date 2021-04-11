8a275bc2ad50b4b4ce7f26b86b934359e166cc04
Akhil Narang <me@akhilnarang.dev>
2021-04-11 17:27:54 +0530

    Initialise the DB inside the container
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/tests-start.sh
+++ app/tests-start.sh
@@ -3,5 +3,11 @@ set -e
 
 module=${1:-app/tests}
 
+# Initialise DB
+alembic upgrade head
+
+# Add in some basic data
 python app/initial_data.py
+
+# Run the tests
 pytest --cov=app --cov-report=term-missing ${module}
