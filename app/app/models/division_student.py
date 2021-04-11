98cc89ffa0cdc12e5785dcc55ec7ea2ec89d9f24
Aditya Desai <aditya.desai0199@gmail.com>
2021-04-11 14:24:31 +0530

    models/division_student: Add a Batch number attribute to the association table between Student and Division

--- app/app/models/division_student.py
+++ app/app/models/division_student.py
@@ -7,4 +7,5 @@ division_student = Table(
     Base.metadata,
     Column("division_id", Integer, ForeignKey("division.id", ondelete="CASCADE"), primary_key=True),
     Column("student_id", Integer, ForeignKey("student.user_id", ondelete="CASCADE"), primary_key=True),
+    Column("batch_number", Integer, index=True),
 )
