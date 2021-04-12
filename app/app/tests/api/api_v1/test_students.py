860788e74cc1ffd8e7672c0ceca1f09770077c8a
ksdfg <ksdfg123@gmail.com>
2021-04-12 21:40:43 +0530

    app: api: endpoints: students.py: Add endpoint to fetch all divisions for a student

--- app/app/tests/api/api_v1/test_students.py
+++ app/app/tests/api/api_v1/test_students.py
@@ -5,9 +5,12 @@ from sqlalchemy.orm import Session
 
 from app import crud
 from app.core.config import settings
+from app.schemas import AdminPermissions
+from app.tests.utils.course import create_random_course
+from app.tests.utils.division import create_random_division
 from app.tests.utils.student import create_random_student
 from app.tests.utils.term import create_random_term
-from app.tests.utils.user import authentication_token_from_email
+from app.tests.utils.user import authentication_token_from_email, create_random_user
 from app.tests.utils.utils import compare_api_and_db_query_results, to_json
 from app.utils import generate_uuid
 
@@ -148,3 +151,80 @@ def test_read_student_by_id_superuser(client: TestClient, superuser_token_header
     fetched_student = r.json()
     assert fetched_student
     compare_api_and_db_query_results(api_result=fetched_student, db_dict=to_json(student))
+
+
+def test_get_student_divisions_id(client: TestClient, db: Session) -> None:
+    student = create_random_student(db)
+    course = create_random_course(db, term_id=student.term_id)
+    divisions = [create_random_division(db, course_id=course.id), create_random_division(db, course_id=course.id)]
+    for division in divisions:
+        division.students.append(student)
+    r = client.get(
+        f"{settings.API_V1_STR}/students/{student.user_id}/divisions",
+        headers=authentication_token_from_email(client=client, email=student.user.email, db=db),
+    )
+    assert r.status_code == 200
+    fetched_divisions = r.json()
+    assert fetched_divisions
+    assert len(fetched_divisions) == 2
+    for division in divisions:
+        # if fetched_divisions[0].get("id") == division.id:
+        #     compare_api_and_db_query_results(api_result=fetched_divisions[0], db_dict=to_json(division))
+        # else:
+        #     compare_api_and_db_query_results(api_result=fetched_divisions[1], db_dict=to_json(division))
+        assert division.id == fetched_divisions[0].get("id") or division.id == fetched_divisions[1].get("id")
+
+
+def test_get_student_divisions_id_admin_with_perms(client: TestClient, db: Session) -> None:
+    student = create_random_student(db)
+    course = create_random_course(db, term_id=student.term_id)
+    division = create_random_division(db, course_id=course.id)
+    division.students.append(student)
+
+    perms = AdminPermissions(0)
+    perms["student"] = True
+    admin = create_random_user(db, type="admin", permissions=perms.permissions)
+
+    r = client.get(
+        f"{settings.API_V1_STR}/students/{student.user_id}/divisions",
+        headers=authentication_token_from_email(client=client, email=admin.email, db=db),
+    )
+    assert r.status_code == 200
+    fetched_divisions = r.json()
+    assert fetched_divisions
+    # compare_api_and_db_query_results(api_result=fetched_divisions[0], db_dict=to_json(division))
+    assert division.id == fetched_divisions[0].get("id")
+
+
+def test_get_student_divisions_id_admin_without_perms(client: TestClient, db: Session) -> None:
+    student = create_random_student(db)
+    admin = create_random_user(db, type="admin")
+    r = client.get(
+        f"{settings.API_V1_STR}/students/{student.user_id}/divisions",
+        headers=authentication_token_from_email(client=client, email=admin.email, db=db),
+    )
+    assert r.status_code == 403
+
+
+def test_get_student_divisions_id_normal_user(
+    client: TestClient, normal_user_token_headers: Dict[str, str], db: Session
+) -> None:
+    student = create_random_student(db)
+    course = create_random_course(db, term_id=student.term_id)
+    division = create_random_division(db, course_id=course.id)
+    division.students.append(student)
+    r = client.get(
+        f"{settings.API_V1_STR}/students/{student.user_id}/divisions",
+        headers=normal_user_token_headers,
+    )
+    assert r.status_code == 403
+
+
+def test_get_student_divisions_id_nonexistent(
+    client: TestClient, normal_user_token_headers: Dict[str, str], db: Session
+) -> None:
+    r = client.get(
+        f"{settings.API_V1_STR}/students/{generate_uuid()}/divisions",
+        headers=normal_user_token_headers,
+    )
+    assert r.status_code == 404
