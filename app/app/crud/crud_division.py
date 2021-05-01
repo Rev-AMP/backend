dcb5e22a0a71836bbee6a21dc01a9330d107dc32
Akhil Narang <me@akhilnarang.dev>
2021-05-01 12:52:49 +0530

    crud_division: fix .filter calls
    Need to chain them or pass the parameters directly, using `and` here does nothing
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/crud/crud_division.py
+++ app/app/crud/crud_division.py
@@ -10,9 +10,7 @@ from app.schemas import DivisionCreate, DivisionUpdate
 class CRUDDivision(CRUDBase[Division, DivisionCreate, DivisionUpdate]):
     def get_by_details(self, db: Session, *, course_id: str, division_code: int) -> Optional[Division]:
         return (
-            db.query(Division)
-            .filter(Division.course_id == course_id and Division.division_code == division_code)
-            .first()
+            db.query(Division).filter(Division.course_id == course_id, Division.division_code == division_code).first()
         )
 
 
