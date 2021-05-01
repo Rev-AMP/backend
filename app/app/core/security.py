2c998bc0bfdcb259ee82b51925288a143fab1f51
Akhil Narang <me@akhilnarang.dev>
2021-05-01 21:31:40 +0530

    Maintain correct value in the response timestamp as well
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/core/security.py
+++ app/app/core/security.py
@@ -17,13 +17,15 @@ def create_token(subject: Union[Any, str]) -> Token:
     :param subject: Subject for JWT, in this case user id
     :return: Token object, containing access and refresh tokens, and the timestamp for the access token's expiry
     """
-    expire = datetime.now() + get_timedelta_for_type("access")
+    access_delta = get_timedelta_for_type("access")
+    expire = datetime.utcnow() + access_delta
+    response_expire = datetime.now() + access_delta
     to_encode = {"exp": expire, "sub": str(subject), "type": "access"}
     access_token = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
-    refresh_expiry = datetime.now() + get_timedelta_for_type("refresh")
+    refresh_expiry = datetime.utcnow() + get_timedelta_for_type("refresh")
     to_encode = {"exp": refresh_expiry, "sub": str(subject), "type": "refresh"}
     refresh_token = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
-    token = Token(access_token=access_token, refresh_token=refresh_token, expiry=int(expire.timestamp()))
+    token = Token(access_token=access_token, refresh_token=refresh_token, expiry=int(response_expire.timestamp()))
     return token
 
 
