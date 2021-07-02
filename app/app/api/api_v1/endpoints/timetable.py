89ff6b95e467b92e133a73431caea74d03320933
Akhil Narang <me@akhilnarang.dev>
2021-07-02 16:03:48 +0530

    api: timetable: allow admins to query by division id
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/app/api/api_v1/endpoints/timetable.py
+++ app/app/api/api_v1/endpoints/timetable.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 
 from app import crud, models
 from app.api import deps
-from app.exceptions import BadRequestException
+from app.exceptions import BadRequestException, NotFoundException
 from app.models import Division
 from app.schemas import Lecture
 
@@ -26,6 +26,18 @@ def get_timetable(
         else:
             raise BadRequestException(detail=f"No timetable can be generated for user type {user.type}")
     raise BadRequestException(detail="User object not found!")
+
+
+@router.get("/{division_id}", response_model=Dict[str, List[Lecture]])
+def get_timetable_division(
+    *,
+    db: Session = Depends(deps.get_db),
+    _: models.User = Depends(deps.get_current_admin_with_permission("course")),
+    division_id: str,
+) -> Any:
+    if division := crud.division.get(db, id=division_id):
+        return generate_timetable(db, [division])
+    raise NotFoundException(detail=f"Division with id {division_id} not found")
 
 
 def generate_timetable(db: Session, divisions: List[Division]) -> Dict:
